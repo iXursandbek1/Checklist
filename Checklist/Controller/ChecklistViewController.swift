cf9677b40a8ace69e98611a30e8e0f12be1b179a
Xursandbek <xursandbek06082001@mail.com>
2023-03-28 16:36:52 +0500

    App saves the data

--- Checklist/Controller/ChecklistViewController.swift
+++ Checklist/Controller/ChecklistViewController.swift
@@ -15,25 +15,9 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        let item1 = ChecklistItem()
-          item1.text = "Walk the dog"
-          items.append(item1)
-          let item2 = ChecklistItem()
-          item2.text = "Brush my teeth"
-          item2.checked = true
-          items.append(item2)
-          let item3 = ChecklistItem()
-          item3.text = "Learn iOS development"
-          item3.checked = true
-          items.append(item3)
-          let item4 = ChecklistItem()
-          item4.text = "Soccer practice"
-          items.append(item4)
-          let item5 = ChecklistItem()
-          item5.text = "Eat ice cream"
-          items.append(item5)
+        navigationItem.largeTitleDisplayMode = .never
+        // Load items
+        loadChecklistItems()
         
         print("Documents folder is \(documentsDirectory())")
         print("Data file path is \(dataFilePath())")
@@ -88,6 +72,8 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
             configureCheckmark(for: cell, with: item)
         }
           tableView.deselectRow(at: indexPath, animated: true)
+        
+        saveChecklistItems()
     }
     
     //MARK: Commit for deleting rows
@@ -98,6 +84,8 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         // 2
           let indexPaths = [indexPath]
           tableView.deleteRows(at: indexPaths, with: .automatic)
+        
+        saveChecklistItems()
     }
     
     func configureCheckmark(
@@ -135,19 +123,21 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         let indexPaths = [indexPath]
         tableView.insertRows(at: indexPaths, with: .automatic)
         navigationController?.popViewController(animated:true)
+        
+        saveChecklistItems()
     }
     
-    func itemDetailViewController(
-      _ controller: ItemDetailViewController,
-      didFinishEditing item: ChecklistItem
-    ){
-    if let index = items.firstIndex(of: item) {
-        let indexPath = IndexPath(row: index, section: 0)
-        if let cell = tableView.cellForRow(at: indexPath) {
-          configureText(for: cell, with: item)
+    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem){
+        
+        if let index = items.firstIndex(of: item) {
+            let indexPath = IndexPath(row: index, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath) {
+            configureText(for: cell, with: item)
         }
     }
-      navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
+            
+        saveChecklistItems()
     }
     
     
@@ -160,6 +150,43 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     func dataFilePath() -> URL {
       return
     documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklistItems() {
+        // 1
+        let encoder = PropertyListEncoder()
+        // 2
+        do {
+        // 3
+            let data = try encoder.encode(items)
+        // 4
+        try data.write(
+            to: dataFilePath(),
+            options: Data.WritingOptions.atomic)
+        // 5
+        } catch {
+        // 6
+            print("Error encoding item array: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklistItems() {
+        // 1
+        let path = dataFilePath()
+        // 2
+        if let data = try? Data(contentsOf: path) {
+        // 3
+        let decoder = PropertyListDecoder()
+        do {
+        // 4
+          items = try decoder.decode(
+            [ChecklistItem].self,
+            from: data)
+        } catch {
+            
+          print("Error decoding item array: \(error.localizedDescription)")}
+            
+      }
     }
     
 }
